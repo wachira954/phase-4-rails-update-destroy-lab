be2bbc952bb1825591e870d365b273abdcf6ca43
wachira <wachiraa954@gmail.com>
2022-10-03 09:57:28 +0300

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,8 @@
 class PlantsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+
   # GET /plants
   def index
     plants = Plant.all
@@ -18,9 +21,33 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # delete
+  def destroy
+    plant = find_plants
+    plant.destroy
+    head :no_content
+  end
+
+  # update
+  def update
+    plant = find_plants
+    plant.update(plant_params)
+    render json: plant
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+
+  def find_plants
+    Plant.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+
 end
